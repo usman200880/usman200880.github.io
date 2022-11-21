867e82b17a5c96bc84206f5886161edc2219a3bf
Usman Sharjeel <i200880@nu.edu.pk>
2022-11-21 23:19:15 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://freepngimg.com/save/9014-john-cena-face-transparent-png/600x637" className="App-logo" alt="JC" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bonus Assignment
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
